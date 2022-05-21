15556be5dc991f62171dc89e2c68f42f70da15c8
Ishan2111 <ishanpandya51@gmail.com>
2022-05-21 14:20:53 +0530

    first commit

---
+++ ecomm-dashboard/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svcdg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-Commerce Project</h1>
+      <button>Normal Button</button>
+      <Button>Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
